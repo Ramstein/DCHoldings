7750e650582768478cf369e2d7b6f41630bd078c
Md <gerialworld@gmail.com>
2022-04-10 06:18:09 +0000

    some changes

--- core/app/Http/Controllers/CheckController.php
+++ core/app/Http/Controllers/CheckController.php
@@ -502,3 +502,4 @@ class CheckController extends Controller
     }
         
 }
+
--- core/storage/framework/views/2345bfb5d6af4b3ebf5e12093ab092ac44fd9935.php
+++ core/storage/framework/views/2345bfb5d6af4b3ebf5e12093ab092ac44fd9935.php
@@ -124,7 +124,7 @@
                             <div class="form-group">
                                 <div class="custom-file">
                                     <input type="file" class="custom-file-input" id="customFileLang" name="image" lang="en" required>
-                                    <label class="custom-file-label" for="customFileLang"><?php echo e(__('Choose Media')); ?></label>
+                                    <label class="custom-file-label" for="customFileLang"><?php echo e(__('Choose Media (Only jpg, png files)')); ?></label>
                                 </div>
                             </div>
                             <div class="form-group row">
